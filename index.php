cade89ee62ea556f93b60ff0ace6439ffca62922
Catherine Igboanugo <79139107+icedesigns923@users.noreply.github.com>
2021-06-24 11:29:58 +0100

    New function
    i created a function that can add more than two numbers

--- index.php
+++ index.php
@@ -3,7 +3,13 @@ $arr = range(0,20);
 
 foreach ($arr as $a){
  echo "$a" . "<br>";
+
 }
+
+// function sum(...$nums){
+//     return array_reduce($nums, fn($carry, $num) => $carry + $num);
+// }
+// echo sum(2,3,3);
 
 function sum($x, $y){
  $z = $x + $y;
