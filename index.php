442eb79eaa901faa5e66f66e27e8fea8c074defc
Catherine Igboanugo <79139107+icedesigns923@users.noreply.github.com>
2021-06-24 10:00:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$arr = range(0,20);
+
+foreach ($arr as $a){
+ echo "$a" . "<br>";
+}
+
+function sum($x, $y){
+ $z = $x + $y;
+ return $z;
+}
+echo 4 + 6 . "<br>";
+echo 5 + 11 . "<br>";
+echo 7 + 8 . "<br>";
+
+
+?>
